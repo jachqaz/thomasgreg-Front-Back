b007f2109761269587ab5694705abe6101598d84
Johan Castaño <jach_1997@hotmail.com>
2024-11-18 09:49:28 -0500

    Initial commit

---
+++ src/main/java/thomasgreg/thomasgreg/CodigoPro00xApplication.java
@@ -0,0 +1,13 @@
+package thomasgreg.thomasgreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoPro00xApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigoPro00xApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thomasgreg/thomasgreg/CodigoPro00xApplicationTests.java
@@ -0,0 +1,13 @@
+package thomasgreg.thomasgreg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoPro00xApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
